b78bd7618f7b82510f9b9a38922cfd0a3eb84997
srooodg <unitsin@yandex.ru>
2023-04-10 21:59:11 +0300

    начало

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -14,7 +14,7 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{
+        return new Class[]{
                 WebConfig.class
         };
     }
---
+++ src/main/java/web/model/Cars.java
@@ -0,0 +1,8 @@
+package web.model;
+
+public class Cars {
+    private String model;
+    private String manufacturingСountry;
+    private Long yearOfRealease;
+
+}
